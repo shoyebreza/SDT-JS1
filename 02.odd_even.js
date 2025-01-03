274499917d05fee778c99bf27b9f6592530ddc2c
shoyeb <68115608+shoyebreza@users.noreply.github.com>
2025-01-03 22:36:05 +0600

    Odd even number

---
+++ 02.odd_even.js
@@ -0,0 +1,13 @@
+
+
+/* # 2. Output Odd/Even Number  */
+
+
+var number = 5;
+
+if (number % 2 === 0) {
+    console.log(number + " is an Even number.");
+} else {
+    console.log(number + " is an Odd number.");
+}
+
