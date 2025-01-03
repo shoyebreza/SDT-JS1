5950950be3521593cc05441597f5ebdf356dbfe8
shoyeb <68115608+shoyebreza@users.noreply.github.com>
2025-01-03 22:54:57 +0600

    remove duplicate number

---
+++ 07.remove_duplicate.js
@@ -0,0 +1,16 @@
+
+/* # 7. find and remove duplicate numbers */
+
+
+var numbers = [1, 2, 3, 3, 4, 4, 5, 6, 7, 8, 9, 10];
+
+var unique = [];
+
+for (var i = 0; i < numbers.length; i++) {
+    if (!unique.includes(numbers[i])) {
+        unique.push(numbers[i]);
+    }
+}
+
+console.log("Array without duplicates: ", unique);
+
